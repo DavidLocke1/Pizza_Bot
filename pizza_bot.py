4ab1b308c681f7bfb2700e7ad77b8da79a235c9f
DavidLocke1 <99290610+DavidLocke1@users.noreply.github.com>
2022-02-13 12:18:22 +1300

    Added welcome function
    Added welcome and main function from component 1 version 2

--- pizza_bot.py
+++ pizza_bot.py
@@ -1,43 +1,68 @@
-# pizza bot program #
+# pizza bot program
+import random
+from random import randint
 
-# welcome message with random name #
-
-
-
-# menu for pick-up or delivery #
-
+# list of random names
+names = ["Mark", "Pheobe", "Sally", "Michael", "Denise", "Ellen", "Eris", "Moana", "Lewis", "Lara"]
 
 
-# pick-up up information - name and phone #
+# welcome message with random name
+def welcome():
+    """
+    Purpose: To genertate a random name from the list and print out a 
+    welcome message
+    Paramaters: None
+    Returns: None
+    """
+    num = randint(0,9)
+    name = (names[num])
+    print("*** Welcome to dream pizza ***")
+    print("*** My name is ",name," ***")
+    print("*** I will be here to help you order your delicious Dream Pizza ***")
 
-
-
-# delivery information - name address and phone number #
-
-
-
-# choose total number of pizzas - max 5 #
-
-
-
-# pizza menu #
-
-
-
-# pizza ordering - from menu - print each pizza ordered with cost #
+# menu for pick-up or delivery
 
 
 
-# print order out - including in deliver or pick-up and names and pizza of each pizza - total cost including any deliver charge #
+# pick-up up information - name and phone
 
 
 
-# ability to cancel or proceed with order #
+# delivery information - name address and phone number
 
 
 
-# option for new order or to exit #
+# choose total number of pizzas - max 5
 
 
 
-# main function #
+# pizza menu
+
+
+
+# pizza ordering - from menu - print each pizza ordered with cost
+
+
+
+# print order out - including in deliver or pick-up and names and pizza of each pizza - total cost including any deliver charge
+
+
+
+# ability to cancel or proceed with order
+
+
+
+# option for new order or to exit
+
+
+
+# main function
+def main():
+    """
+    Purpose: To run all functions
+    Parameters: None
+    Returns: None
+    """
+    welcome()
+
+main()
