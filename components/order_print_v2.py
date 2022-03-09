8eeeeebbb6bb9c36ef8f506b8ecbf14777234bcd
DavidLocke1 <99290610+DavidLocke1@users.noreply.github.com>
2022-03-10 09:02:26 +1300

    print customer details
    added code to print customer details

--- components/order_print_v2.py
+++ components/order_print_v2.py
@@ -14,6 +14,12 @@ order_cost =[8.50,13.50,13.50,5.50]
 
 customer_details = {'name': 'Mark', 'phone': '0123456789', 'house': '34', 'street': 'Ones', 'suburb': 'Twos'}
 
+
+print(customer_details['name'],"\n",customer_details['phone'],"\n",customer_details['house'],"\n",customer_details['street'],"\n",customer_details['suburb'])
+
+print("\nCustomer Name: {} Customer Phone: \n{} Customer House Number:\n{} Customer Street Name:\n{} Customer Suburb:\n{}".format(customer_details['name'],customer_details['phone'],customer_details['house'],customer_details['street'],customer_details['suburb']))
+
+
 count = 0
 for item in order_list:
     print("Ordered: {} Cost ${:.2f}".format(item, order_cost[count]))
